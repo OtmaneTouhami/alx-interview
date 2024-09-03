099b2bb29ed9ff087435f0c349c46817415e2062
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-09-03 17:55:12 +0100

    0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
+const util = require('util');
 
-// Function to get character name from URL
-function getCharacterName(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const character = JSON.parse(body);
-        resolve(character.name);
-      }
-    });
-  });
-}
+// Convert request to return a promise
+const requestPromise = util.promisify(request);
 
-// Main function to get and print character names
-async function printCharacterNames(movieId) {
-  const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-  request(movieUrl, async (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      return;
+(async function () {
+  try {
+    // Make API request for the film
+    const filmRes = await requestPromise(filmURL + filmNum);
+    const charURLList = JSON.parse(filmRes.body).characters;
+
+    // Use URL list to make requests for each character, in order
+    for (const charURL of charURLList) {
+      const charRes = await requestPromise(charURL);
+      console.log(JSON.parse(charRes.body).name);
     }
-
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    for (const url of characterUrls) {
-      try {
-        const name = await getCharacterName(url);
-        console.log(name);
-      } catch (error) {
-        console.error('Error fetching character:', error);
-      }
-    }
-  });
-}
-
-// Get movie ID from command line argument
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.log('Please provide a movie ID');
-} else {
-  printCharacterNames(movieId);
-}
+  } catch (err) {
+    console.error(err);
+  }
+})();
